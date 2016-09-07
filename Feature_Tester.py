e3515260c47224bbc0bb3da4cfd366841c02fca1
Jeremy Coupland <jcoupland12@gmail.com>
2016-09-07 18:08:26 +0000

    Added a take photo function, which takes a file name as an argument, takes a photo and returns the saved image file path as a string. Then add a function to detect the colour of an image, which takes in the file path, iterates over the image and determines whether the image is red, green, or blue

--- Feature_Tester.py
+++ Feature_Tester.py
@@ -3,15 +3,20 @@ import time
 
 
 def main():
-    Features.IR_DetectObstacle()
-        
-    time.sleep(2)
+#    Features.IR_DetectObstacle()
+#       
+#    time.sleep(2)
+#
+#    Features.IR_WaitForObstacle()
+#    
+#    Features.CAM_TakePhoto("red", 180)    
 
-    Features.IR_WaitForObstacle()
+    Features.CAM_DetectColour("red1")
+    Features.CAM_DetectColour("red2")
+    Features.CAM_DetectColour("red3")
+    Features.CAM_DetectColour("green1")
+    Features.CAM_DetectColour("blue1")
+
     
-
-
-
-
 
 main()
--- Robot_Features.py
+++ Robot_Features.py
@@ -1,13 +1,16 @@
 import RPi.GPIO as GPIO
 import time
-
+from picamera import PiCamera
+import os
+from PIL import Image
 
 
 def IR_DetectObstacle():
     GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(3, GPIO.IN)
+    inPin = 3
+    GPIO.setup(inPin, GPIO.IN)
 
-    if (GPIO.input(3) == 1):
+    if (GPIO.input(inPin) == 1):
         print("No obstacles detected")
         return False
 
@@ -16,20 +19,94 @@ def IR_DetectObstacle():
         return True
 
     GPIO.cleanup()
+    
 
 
 def IR_WaitForObstacle():
     GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(3, GPIO.IN)
+    inPin = 3
+    GPIO.setup(inPin, GPIO.IN)
 
     while (True):
-        if (GPIO.input(3) == 0):
+        if (GPIO.input(inPin) == 0):
             print("Obstacle detected!")
             break
         
         time.sleep(0.05)
     
     GPIO.cleanup()
+
     
+    
+def CAM_TakePhoto(fileName, rotation = 0, sleepTime = 2):
+    filePath = ('/home/pi/RASPIED/Camera_Images/' + str(fileName)+ '.jpg')
+    
+    if os.path.isfile(filePath) == True:
+        os.remove(filePath)
+        
+    camera = PiCamera()
+    camera.resolution = (300, 300)
+    camera.rotation = rotation
+    camera.start_preview()
+    time.sleep(sleepTime)
+    camera.capture(filePath)
+    camera.stop_preview()
+    print("Photo taken")
+
+    return filePath
+    
+
+def CAM_DetectColour(fileName):
+    filePath = ('/home/pi/RASPIED/Camera_Images/' + str(fileName)+ '.jpg')
+
+    if os.path.isfile(filePath) == False:
+        print("Error - file '" + fileName + "' not found!")
+
+    else:
+        im = Image.open(filePath)
+        pixels = im.load()
+        x = im.size[0]
+        y = im.size[1]
+        totalPixels = x*y
+
+        avgR = 0
+        avgG = 0
+        avgB = 0
+
+        for i in range (0, x):          # maybe optimise to look at 100-1000 random pixels, rather than all of them
+            for j in range (0, y):
+                avgR = avgR + pixels[i, j][0]
+                avgG = avgG + pixels[i, j][1]
+                avgB = avgB + pixels[i, j][2]
+                
+        avgR = avgR / totalPixels
+        avgG = avgG / totalPixels
+        avgB = avgB / totalPixels
+        
+        if (avgR >= 200 and ((avgG + avgB)/2) < 160):
+            print("Image '" + fileName + "' colour is red!")
+            return "red"
+
+        if (avgG >= 200 and ((avgR + avgB)/2) < 160):
+            print("Image '" + fileName + "' colour is green!")
+            return "green"
+
+        if (avgB >= 200 and ((avgR + avgG)/2) < 160):
+            print("Image '" + fileName + "' colour is blue!")
+            return "blue"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     
